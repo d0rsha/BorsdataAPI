ead0a92943301ef4718c64bf6be7018149755aae
Anders Vrethem <avrethem@gmail.com>
2021-07-13 22:44:51 +0200

    Excel_updater.py fixes

--- borsdata/excel_updater.py
+++ borsdata/excel_updater.py
@@ -22,6 +22,7 @@ class ExcelUpdater:
         self._api._set_index(self.last_prices, ['date', 'insId'], ascending=False)
         with open(constants.EXPORT_PATH  + 'last_update.txt', "r") as f:
             self.last_update = f.readlines()[0]
+        self.last_update = pd.to_datetime(last_update)
         self.new_update = self.dates[0]
 
 
@@ -39,45 +40,44 @@ class ExcelUpdater:
 
         reports_r12 = pd.read_excel(open(os.path.join(root, file), 'rb'), sheet_name='reports_r12')
         reports_r12.fillna(method='ffill', axis=0, inplace=True)
-        self._api._set_index(reports_r12, ['year', 'period'], ascending=False)
         reports_r12['year'] = pd.to_numeric(reports_r12['year'], downcast='integer')
+        self._api._set_index(reports_r12, ['year', 'period'], ascending=False)
 
         stock_meta = pd.read_excel(open(os.path.join(root, file), 'rb'), sheet_name='meta_data', index_col=0)
 
         return stock_prices, reports_quarter, reports_year, reports_r12, stock_meta
 
     def get_date_stock_price(self, date, insId):
-        if dates.__contains__(date) == False:
-            self.last_prices = self.last_prices.append(self._api.get_stock_prices_date(date))
+        if self.dates.__contains__(date) == False:
+            prices = self._api.get_stock_prices_date(date.strftime('%Y-%m-%d'))
+            prices.reset_index(inplace=True)
+            self._api._set_index(prices, ['date', 'insId'], ascending=False)
+            self.last_prices = self.last_prices.append(prices)
             self.dates.append(date)
-        
+
         return self.last_prices.loc[(date, insId)]
         
     def excel_export(self, stock_prices, reports_quarter, reports_year, reports_r12, stock_meta, ):
-        export_path = constants.EXPORT_PATH + f"{stock_meta['country']}/{stock_meta['market']}/"
+        export_path = constants.EXPORT_PATH + f"{stock_meta['country'].loc[0]}/{stock_meta['market'].loc[0]}/"
 
         # creating path if not exists
         if not os.path.exists(export_path):
             os.makedirs(export_path)
 
         # creating the writer with export location
-        excel_writer = pd.ExcelWriter(export_path + stock_meta['name'] + ".xlsx")
+        excel_writer = pd.ExcelWriter(export_path + stock_meta['name'].loc[0] + ".xlsx")
         stock_prices.to_excel(excel_writer, 'stock_prices')
         reports_quarter.to_excel(excel_writer, 'reports_quarter')
         reports_year.to_excel(excel_writer, 'reports_year')
         reports_r12.to_excel(excel_writer, 'reports_r12')
         stock_meta.to_excel(excel_writer, 'meta_data')
         excel_writer.save()
-        print(f"Excel exported: {export_path + stock_meta['name'] + '.xlsx'}")
+        print(f"Excel exported: {export_path + stock_meta['name'].loc[0] + '.xlsx'}")
 
     def update_excel_files(self):
         path = os.getcwd() + "\\" + self._file_path
 
-        # get last updated at 
-        old_update_date = self.last_update
-        new_update_date = self.new_update
-
-        if (old_update_date == new_update_date): 
+        if (self.last_update >= self.new_update): 
             return
         for root, dirs, files in os.walk(path):
             for file in files:
@@ -87,23 +87,22 @@ class ExcelUpdater:
                     insId = stock_meta['insId'][0]
 
                     # Compare, get weekdays to add 
-                    daterange = pd.date_range(old_update_date, new_update_date, freq='D').to_series()
+                    start_date = self.last_update if (self.last_update > stock_prices.index[0]) else stock_prices.index[0]
+                    daterange = pd.date_range(start_date, self.new_update, freq='D').to_series()
                     daterange = daterange[daterange.dt.dayofweek.isin([0, 1, 2, 3, 4])]
 
                     # Remove first date, we already got it
                     daterange = daterange[1:]
 
                     for date in daterange:
-                        stock_prices[date] = get_date_stock_price(insId, date)
+                        stock_prices.loc[date] = get_date_stock_price(date, insId)
 
-                    stock_meta['ohlcv_updated'] = new_update_date
+                    stock_meta['ohlcv_updated'] = self.new_update
                     self.excel_export(stock_prices, reports_quarter, reports_year, reports_r12, stock_meta)
 
-        print(f"Set last update {new_update_date} in {constants.EXPORT_PATH + 'last_update.txt'}")
+        print(f"Set last update {self.new_update} in {constants.EXPORT_PATH + 'last_update.txt'}")
         with open(constants.EXPORT_PATH  + 'last_update.txt', "w") as f:
-            f.write(f'{new_update_date}')
-                    
-
+            f.write(f'{self.new_update}')
 
 
 
